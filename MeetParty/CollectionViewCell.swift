1c020171327af84e976009ff61678f44c5e15cfd
khj <khjs0806@naver.com>
2018-12-24 17:19:29 +0900

    first commit

---
+++ MeetParty/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MeetParty
+//
+//  Created by 방문 사용자 on 24/12/2018.
+//  Copyright © 2018 KHJ. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
