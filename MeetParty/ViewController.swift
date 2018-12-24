5b699c2b49ab654d69600478741e858c6b05fc01
방문 사용자 <Guest@satanui-MacBook-Pro.local>
2018-12-24 12:23:31 +0900

    Initial Commit

---
+++ MeetParty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetParty
+//
+//  Created by 방문 사용자 on 24/12/2018.
+//  Copyright © 2018 KHJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
